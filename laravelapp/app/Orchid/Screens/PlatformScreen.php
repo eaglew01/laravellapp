414ff5357d6ddbce08fa02220d890f4e2f460db3
eaglew01 <wouter.de.cleer@student.ehb.be>
2023-11-11 11:31:08 +0100

    Finetune Admin Pannel

--- laravelapp/app/Orchid/Screens/PlatformScreen.php
+++ laravelapp/app/Orchid/Screens/PlatformScreen.php
@@ -32,7 +32,7 @@ class PlatformScreen extends Screen
      */
     public function description(): ?string
     {
-        return 'Welcome to your Orchid application.';
+        return 'Welcome to your ADMIN PANNEL';
     }
 
     /**
@@ -53,8 +53,8 @@ class PlatformScreen extends Screen
     public function layout(): iterable
     {
         return [
-            Layout::view('platform::partials.update-assets'),
-            Layout::view('platform::partials.welcome'),
+            Layout::view('adminPannel.update-assets'),
+            Layout::view('adminPannel.mywelcome'),
         ];
     }
 }
---
+++ laravelapp/resources/views/adminPannel/mywelcome.blade.php
@@ -0,0 +1,28 @@
+<div class="bg-white rounded-top shadow-sm mb-4 rounded-bottom">
+
+    <div class="row g-0">
+        <div class="col col-lg-7 mt-6 p-4">
+
+            <h2 class="text-dark fw-light lh-lg">
+                Hello, It’s Great to See You!
+            </h2>
+
+            <p>
+                On the left menu you find all the CRUDS:
+                <br>
+                => CRUD for users + roles
+                <br>
+                => CRUD for categories that is a foreign key to FAQS
+                <br>
+                => CRUD for FAQS
+                <br>
+                => CRUD for Vacancies
+
+            </p>
+        </div>
+        
+
+
+
+    </div>
+</div>
